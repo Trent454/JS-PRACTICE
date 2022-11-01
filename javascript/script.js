63057be76f69573340c65ad3b7a0f9c166be2857
Trent454 <113075475+Trent454@users.noreply.github.com>
2022-10-31 21:15:48 -0700

    class update

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+let name = "john";
+let age = 21;
+
+const interestRate = 2.5;
+
+age = 48
+interestRate = 3.5;
+
+console.log(age);
+console.log(name);
+console.log(interestRate);
+
+/*
+  This is how you do multi-line comment 
+
+
+ */
