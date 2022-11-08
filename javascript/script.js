b30fe23c5148be6d53006bf65fe5ad538fafba2d
Trent454 <113075475+Trent454@users.noreply.github.com>
2022-11-08 13:36:39 -0800

    day 2

--- javascript/script.js
+++ javascript/script.js
@@ -2,21 +2,19 @@ let name = "Trenton";
 let age = 28;
 let favfood ="Tamales";
 
-let interestRate = 2.5;
 
-age = 48
-interestRate = 3.5;
-
-console.log(age);
 console.log(name);
-console.log(interestRate);
+console.log(age);
+console.log(favfood);
 
 /*
   This is how you do multi-line comment 
  */
 // This is my single line comment
 
-let word = "supercalifragilistic expialidocious"
+let word = "supercalifragilisticexpicewcious"
 console.log (word.length)
-console.log(55)
-console.log(1010101010101010101010101010101n)
+let car = " ROYGEBIV"
+
+
+
