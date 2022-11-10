f7c0e23aeb1b1b2d537324c10febcea5ff016aa6
Trent454 <113075475+Trent454@users.noreply.github.com>
2022-11-09 21:36:37 -0800

    day 4

--- javascript/script.js
+++ javascript/script.js
@@ -1,27 +1,55 @@
-let name = "Trenton";
-let age = 28;
-let favfood ="Tamales";
+// let name = "Trenton";
+// let age = 28;
+// let favfood ="Tamales";
 
 
-console.log(name);
-console.log(age);
-console.log(favfood);
+// console.log(name);
+// console.log(age);
+// console.log(favfood);
 
-/*
-  This is how you do multi-line comment 
- */
-// This is my single line comment
+// /*
+//   This is how you do multi-line comment 
+//  */
+// // This is my single line comment
 
-let word = "supercalifragilisticexpicewcious"
-console.log (word.length)
-let car = " ROYGEBIV"
+// let word = "supercalifragilisticexpicewcious"
+// console.log (word.length)
+// let car = " ROYGEBIV"
 
-let firstname = prompt("First name")
-let lastname = prompt("last name")
-console.log(firstname)
-alert(lastname)
+// let firstname = prompt("First name")
+// let lastname = prompt("last name")
+// console.log(firstname)
+// alert(lastname)
 
-let birthday = prompt("birthday")
-confirm(birthday)
-alert(birthday)
+// let birthday = prompt("birthday")
+// confirm(birthday)
+// alert(birthday)
 
+let num1 = prompt('Enter a number');
+num1 = parseFloat(num1);
+
+console.log(`num1: ${num1}`);
+
+if (isNaN(num1)) {
+  console.log('num1 was NaN, asking again!')
+  num1 = prompt('You entered a non number. Please enter a number');
+  num1 = parseFloat(num1);
+}
+
+let num2 = prompt('Enter a number');
+num2 = parseFloat(num2);
+
+console.log(num2);
+
+if (isNaN(num2)) {
+  num2 = prompt('You entered a non number. Please enter a number');
+  num2 = parseFloat(num2);
+}
+
+if (num1 === num2) {
+  console.log('the numbers are equal');
+} else if (num1 > num2) {
+  console.log(`num1 was greater: ${num1}`);
+} else {
+  console.log(`num2 was greater: ${num2}`);
+}
