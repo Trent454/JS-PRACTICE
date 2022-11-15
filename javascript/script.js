bfc118892a685a47923cea2720ff6b21b38a83e3
Trent454 <113075475+Trent454@users.noreply.github.com>
2022-11-15 10:35:08 -0800

    update js

--- javascript/script.js
+++ javascript/script.js
@@ -25,31 +25,32 @@
 // confirm(birthday)
 // alert(birthday)
 
-let num1 = prompt('Enter a number');
-num1 = parseFloat(num1);
+// let num1 = prompt('Enter a number');
+// num1 = parseFloat(num1);
 
-console.log(`num1: ${num1}`);
+// console.log(`num1: ${num1}`);
 
-if (isNaN(num1)) {
-  console.log('num1 was NaN, asking again!')
-  num1 = prompt('You entered a non number. Please enter a number');
-  num1 = parseFloat(num1);
-}
+// if (isNaN(num1)) {
+//   console.log('num1 was NaN, asking again!')
+//   num1 = prompt('You entered a non number. Please enter a number');
+//   num1 = parseFloat(num1);
+// }
 
-let num2 = prompt('Enter a number');
-num2 = parseFloat(num2);
+// let num2 = prompt('Enter a number');
+// num2 = parseFloat(num2);
 
-console.log(num2);
+// console.log(num2);
 
-if (isNaN(num2)) {
-  num2 = prompt('You entered a non number. Please enter a number');
-  num2 = parseFloat(num2);
-}
+// if (isNaN(num2)) {
+//   num2 = prompt('You entered a non number. Please enter a number');
+//   num2 = parseFloat(num2);
+// }
 
-if (num1 === num2) {
-  console.log('the numbers are equal');
-} else if (num1 > num2) {
-  console.log(`num1 was greater: ${num1}`);
-} else {
-  console.log(`num2 was greater: ${num2}`);
-}
+// if (num1 === num2) {
+//   console.log('the numbers are equal');
+// } else if (num1 > num2) {
+//   console.log(`num1 was greater: ${num1}`);
+// } else {
+//   console.log(`num2 was greater: ${num2}`);
+// }
+
